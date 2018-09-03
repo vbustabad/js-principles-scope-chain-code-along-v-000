2650080ed23fcfe17366fe55dea90356d39bde12
Veronica Bustabad <vbustabad@gmail.com>
2018-09-03 16:14:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var customerName = 'bob'
-const leastFavoriteCustomer = 'sour fred'
+var customerName = 'bob';
+const leastFavoriteCustomer = 'sour fred';
 
 function upperCaseCustomerName() {
   return customerName = customerName.toUpperCase()
